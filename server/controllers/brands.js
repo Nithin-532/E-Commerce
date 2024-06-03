3595f0282f64ed4554cde8e5f835c91bb2edde94
Hari <harinarayanmaddikari@email.com>
2024-06-03 22:00:07 +0530

    Add controllers

---
+++ server/controllers/brands.js
@@ -0,0 +1,12 @@
+
+
+const getUserPosts = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const post = await Post.find({ userId });
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+  
