f32fb6127d15bdcedf14c01b5ff25fa0e44ac79a
Nithin-532 <kallurunithin8771@gmail.com>
2024-07-11 22:37:25 +0530

    no bootstrap

--- frontend/src/Components/PrevFooter/PrevFooter.js
+++ frontend/src/Components/PrevFooter/PrevFooter.js
@@ -1,7 +1,5 @@
 // src/ContactSection.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const ContactSection = () => {
   return (
